4d11222ba3358675d63544c9e5485425ae30e6ab
Hizar62 <hizarsajjad632@gmail.com>
2023-08-06 22:15:31 +0500

    Adding Detail Screen UI

--- app/src/main/java/com/example/movieui/core/route/AppRoute.kt
+++ app/src/main/java/com/example/movieui/core/route/AppRoute.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.movieui.module.detail.presentation.DetailScreen
 import com.example.movieui.module.home.model.nowPlayingMovie
 import com.example.movieui.module.home.presentation.HomeScreen
+import com.example.movieui.module.seat_selector.presentation.SeatSelectorScreen
 
 object AppRoute {
 
@@ -16,14 +18,17 @@ object AppRoute {
             startDestination = AppRouteName.Home,
         ) {
             composable(AppRouteName.Home) {
-               HomeScreen(navController = navController)
+                HomeScreen(navController = navController)
             }
             composable("${AppRouteName.Detail}/{id}") { backStackEntry ->
                 val id = backStackEntry.arguments?.getString("id")
                 val movie = nowPlayingMovie.first{ it.id == id }
 
+                DetailScreen(navController = navController, movie)
             }
-
+            composable(AppRouteName.SeatSelector) {
+                SeatSelectorScreen(navController = navController)
+            }
         }
     }
 }
--- app/src/main/java/com/example/movieui/core/route/AppRouteName.kt
+++ app/src/main/java/com/example/movieui/core/route/AppRouteName.kt
@@ -3,4 +3,5 @@ package com.example.movieui.core.route
 object AppRouteName {
     const val Home = "/home"
     const val Detail = "/detail"
+    const val SeatSelector = "/seat-selector"
 }
